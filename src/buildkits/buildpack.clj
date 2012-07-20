8070015af8650b0c7dd7145757538c987ed6fd8a
Phil Hagelberg <technomancy@gmail.com>
2012-07-19 17:07:05 -0700

    Support share and unshare.

--- src/buildkits/buildpack.clj
+++ src/buildkits/buildpack.clj
@@ -47,7 +47,7 @@
     (s3-put org buildpack-name bytes)
     {:status 201 :body (json/encode {:revision rev-id})}))
 
-(defn update [username org buildpack _ content]
+(defn update [username org buildpack content]
   (let [bytes (get-bytes content)
         rev-id (db/update org (:id buildpack) bytes)]
     (s3-put org (:name buildpack) bytes)
@@ -63,7 +63,7 @@
 (defn rollback [username org buildpack target]
   (sql/with-query-results [rev] (rollback-query org (:id buildpack) target)
     (if [rev]
-      (update username org buildpack nil (:tarball rev))
+      (update username org buildpack (:tarball rev))
       {:status 404})))
 
 (defn revisions [_ _ buildpack]
@@ -73,20 +73,48 @@
                                 (:id buildpack)]
     {:status 200 :body (json/encode revs)}))
 
+(defn share [_ org email]
+  (sql/with-query-results [{:keys [id]}]
+    ["SELECT * FROM organizations WHERE name = ?" org]
+    ;; TODO: ensure user exists
+    (sql/with-query-results [member] [(str "SELECT * FROM memberships WHERE"
+                                           " email = ? AND organization_id = ?")
+                                      email id]
+      (if member
+        {:status 409}
+        (do (sql/insert-record "memberships" {:email email :organization_id id})
+            {:status 201})))))
+
+(defn unshare [_ org email]
+  (sql/with-query-results [membership]
+    [(str "SELECT * FROM memberships, organizations WHERE email = ? AND"
+          " organization_id = organizations.id AND organizations.name = ?")
+     email org]
+    (if membership
+      (do (sql/delete-rows "memberships" ["id = ?" (:id membership)])
+          {:status 200})
+      {:status 404})))
+
 (def ^:dynamic *not-found* (constantly {:status 404}))
 
-(defn check-auth [headers org buildpack-name found & args]
+(defn check-auth [headers org callback & args]
   (if-let [authorization (get headers "authorization")]
     (let [[username key] (-> authorization (.split " ") second
                              .getBytes base64/decode String. (.split ":"))]
       (sql/with-connection db/db
-        (if (and (check-api-key username key)
-                 (org-member? username org))
-          (if-let [pack (db/get-buildpack org buildpack-name)]
-            (apply found username org pack args)
-            (apply *not-found* username org args))
+        (if (check-api-key username key)
+          (if (org-member? username org)
+            (apply callback username org args)
+            {:status 403})
           {:status 401})))
     {:status 401}))
+
+(defn check-pack-auth [headers org buildpack-name callback & args]
+  (let [pack-callback (fn [username org & args]
+                        (if-let [pack (db/get-buildpack org buildpack-name)]
+                          (apply callback username org pack args)
+                          (apply *not-found* username org args)))]
+    (apply check-auth headers org pack-callback args)))
 
 (defroutes app
   (GET "/buildpacks" []
@@ -95,11 +123,17 @@
                 (json/encode (db/get-buildpacks)))})
   (GET "/buildpacks/:org/:name/revisions" {{:keys [org name]} :params
                                            headers :headers}
-       (check-auth headers org name revisions))
+       (check-pack-auth headers org name revisions))
   (POST "/buildpacks/:org/:name" {{:keys [org name buildpack]} :params
                                   headers :headers}
         (binding [*not-found* create]
-          (check-auth headers org name update name (:tempfile buildpack))))
+          (check-pack-auth headers org name update (:tempfile buildpack))))
   (POST "/buildpacks/:org/:name/revisions/:target"
         {{:keys [org name target]} :params headers :headers}
-        (check-auth headers org name rollback target)))
+        (check-pack-auth headers org name rollback target))
+  (POST "/buildpacks/:org/share/:email" {{:keys [org email]} :params
+                                         headers :headers}
+        (check-auth headers org share email))
+  (DELETE "/buildpacks/:org/share/:email" {{:keys [org email]} :params
+                                           headers :headers}
+          (check-auth headers org unshare email)))
